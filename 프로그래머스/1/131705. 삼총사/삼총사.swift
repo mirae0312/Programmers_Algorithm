7bd1d7d0883a1773257529c0445bff37c6d91edb
Mirae Kim <77021071+mirae0312@users.noreply.github.com>
2023-12-01 20:16:32 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func solution(_ number:[Int]) -> Int {
+    var answer = 0
+    
+    for i in 0..<(number.count - 2) {
+        for j in (i + 1)..<(number.count - 1) {
+            for k in (j + 1)..<(number.count) {
+                if number[i] + number[j] + number[k] == 0 {
+                    answer += 1
+                }
+            }
+        }
+    }
+    return answer
+}

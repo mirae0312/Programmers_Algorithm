bf1916b6f780a749012073d194ea6eebf3289e70
Mirae Kim <77021071+mirae0312@users.noreply.github.com>
2024-01-04 20:21:25 +0900

    [level unrated] Title: 각도기, Time: 0.00 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120829. 각도기/README.md
+++ 프로그래머스/unrated/120829. 각도기/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 각도기 - 120829 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120829?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120829) 
 
 ### 성능 요약
 
-메모리: 16.5 MB, 시간: 0.00 ms
+메모리: 16.4 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 2일 17:47:6
+2024년 1월 4일 20:21:23
 
 ### 문제 설명
 
--- 프로그래머스/unrated/120829. 각도기/각도기.swift
+++ 프로그래머스/unrated/120829. 각도기/각도기.swift
@@ -1,14 +1,16 @@
 import Foundation
 
 func solution(_ angle:Int) -> Int {
-    if angle < 90 {
-        return 1
-    } else if angle == 90 {
-        return 2
-    } else if angle < 180 {
-        return 3
-    } else {
-        return 4
-    }
-    return 0;
+    // if angle < 90 {
+    //     return 1
+    // } else if angle == 90 {
+    //     return 2
+    // } else if angle < 180 {
+    //     return 3
+    // } else {
+    //     return 4
+    // }
+    // return 0;
+    
+    return angle < 90 ? 1 : angle == 90 ? 2 : angle < 180 ? 3 : angle == 180 ? 4 : 0
 }
